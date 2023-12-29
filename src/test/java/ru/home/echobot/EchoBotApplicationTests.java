17160411d8d94c883734563ea0f6615d905aa588
pashtetrus33 <testerrus33@gmail.com>
2023-12-29 04:23:10 +0300

    spring course hw1

---
+++ src/test/java/ru/home/echobot/EchoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.echobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
